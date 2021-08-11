2fb2157548cc24289c3e3f885c79368dbe126d21
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2021-08-11 20:22:45 +0900

    up

--- test/aws-cdk-node.test.ts
+++ test/aws-cdk-node.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as AwsCdkNode from '../lib/aws-cdk-node-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new AwsCdkNode.AwsCdkNodeStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new AwsCdkNode.AwsCdkNodeStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
