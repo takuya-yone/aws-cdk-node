1dedb4d3f39b6efda6ba078abd27d36e3829b3a7
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2021-08-12 01:32:17 +0000

    up

--- lib/aws-cdk-node-stack.ts
+++ lib/aws-cdk-node-stack.ts
@@ -4,9 +4,11 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 export class AwsCdkNodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const vpcCidr = '10.0.0.0/16'
 
     const vpc = new ec2.Vpc(this, 'VPC', {
-      cidr: '10.0.0.0/16',
+      cidr: vpcCidr,
       subnetConfiguration: [
         {
           cidrMask: 24,
@@ -20,5 +22,45 @@ export class AwsCdkNodeStack extends cdk.Stack {
         },
       ],
     });
+    
+    
+    ////////////////// Public Security Group ////////////////// 
+    
+
+    const proxyIP1 = '0.0.0.0/32'
+    const proxyIP2 = '0.0.0.0/32'
+
+    const pubSecurityGroup = new ec2.SecurityGroup(this, 'PubSG', {
+      vpc: vpc,
+      allowAllOutbound: true,
+      securityGroupName: 'PubSG'
+    });
+    
+    
+    // Ingress Rule
+    pubSecurityGroup.addIngressRule(ec2.Peer.ipv4(proxyIP1), ec2.Port.allTraffic());
+    pubSecurityGroup.addIngressRule(ec2.Peer.ipv4(proxyIP2), ec2.Port.allTraffic());
+    
+    ////////////////// ////////////////// ////////////////// 
+   
+    ////////////////// Public Security Group ////////////////// 
+    
+    const privateSecurityGroup = new ec2.SecurityGroup(this, 'PrivateSG', {
+      vpc: vpc,
+      allowAllOutbound: true,
+      securityGroupName: 'PrivateSG',
+    });
+    
+    // Ingress Rule
+    privateSecurityGroup.addIngressRule(ec2.Peer.ipv4(vpcCidr), ec2.Port.allTraffic());
+    
+    ////////////////// ////////////////// ////////////////// 
+   
+   
+   
+   
+   
+   
+    
   }
 }
