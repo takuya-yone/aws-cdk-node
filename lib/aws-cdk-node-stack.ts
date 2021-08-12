f4a416897e27af49c7d3a8a97df5640683abe4ac
Takuya Yonezawa <yonezawa.takuya.yq2@is.naist.jp>
2021-08-12 16:44:44 +0000

    up

--- lib/aws-cdk-node-stack.ts
+++ lib/aws-cdk-node-stack.ts
@@ -4,8 +4,8 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 export class AwsCdkNodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
-    const vpcCidr = '10.0.0.0/16'
+
+    const vpcCidr = '10.0.0.0/16';
 
     const vpc = new ec2.Vpc(this, 'VPC', {
       cidr: vpcCidr,
@@ -22,45 +22,45 @@ export class AwsCdkNodeStack extends cdk.Stack {
         },
       ],
     });
-    
-    
-    ////////////////// Public Security Group ////////////////// 
-    
 
-    const proxyIP1 = '0.0.0.0/32'
-    const proxyIP2 = '0.0.0.0/32'
+    ////////////////// Public Security Group //////////////////
 
-    const pubSecurityGroup = new ec2.SecurityGroup(this, 'PubSG', {
+    const proxyIP1 = '0.0.0.0/32';
+    const proxyIP2 = '0.0.0.0/32';
+
+    const pubSecurityGroup = new ec2.SecurityGroup(this, 'PublicSG', {
       vpc: vpc,
       allowAllOutbound: true,
-      securityGroupName: 'PubSG'
+      securityGroupName: 'PublicSG',
     });
-    
-    
+
     // Ingress Rule
-    pubSecurityGroup.addIngressRule(ec2.Peer.ipv4(proxyIP1), ec2.Port.allTraffic());
-    pubSecurityGroup.addIngressRule(ec2.Peer.ipv4(proxyIP2), ec2.Port.allTraffic());
-    
-    ////////////////// ////////////////// ////////////////// 
-   
-    ////////////////// Public Security Group ////////////////// 
-    
+    pubSecurityGroup.addIngressRule(pubSecurityGroup, ec2.Port.allTraffic());
+    pubSecurityGroup.addIngressRule(
+      ec2.Peer.ipv4(proxyIP1),
+      ec2.Port.allTraffic()
+    );
+    pubSecurityGroup.addIngressRule(
+      ec2.Peer.ipv4(proxyIP2),
+      ec2.Port.allTraffic()
+    );
+
+    ////////////////// ////////////////// //////////////////
+
+    ////////////////// Public Security Group //////////////////
+
     const privateSecurityGroup = new ec2.SecurityGroup(this, 'PrivateSG', {
       vpc: vpc,
       allowAllOutbound: true,
       securityGroupName: 'PrivateSG',
     });
-    
+
     // Ingress Rule
-    privateSecurityGroup.addIngressRule(ec2.Peer.ipv4(vpcCidr), ec2.Port.allTraffic());
-    
-    ////////////////// ////////////////// ////////////////// 
-   
-   
-   
-   
-   
-   
-    
+    privateSecurityGroup.addIngressRule(
+      ec2.Peer.ipv4(vpcCidr),
+      ec2.Port.allTraffic()
+    );
+
+    ////////////////// ////////////////// //////////////////
   }
 }
